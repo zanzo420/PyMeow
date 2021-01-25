4064e41e38b38b0e4bfded741cb7016a440734f8
Shucks <shucks@web.com>
2021-01-25 11:58:53 +0100

    iterating modules

--- examples/get_processes.py
+++ examples/get_processes.py
@@ -1,5 +1,6 @@
 from pymeow import enumerate_processes
 
 for proc in enumerate_processes():
-    modules = ", ".join(list(proc["modules"].keys()))
-    print(f"[{proc['name']}] Modules: {modules}")
+    print(f"[{proc['name']}]")
+    for module, module_data in proc["modules"].items():
+        print(f"\t{module}: {hex(module_data['baseaddr'])}")
