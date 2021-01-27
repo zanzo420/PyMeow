ca48fbf46828575266409992e23d749ea4394600
Shucks <shucks@web.com>
2021-01-27 20:44:47 +0100

    .

--- examples/csgo_esp.py
+++ examples/csgo_esp.py
@@ -93,7 +93,11 @@ def main():
             view_matrix = read_floats(csgo_proc, game_module + Offsets.dwViewMatrix, 16)
             for ent_addr in ent_addrs:
                 if ent_addr > 0 and ent_addr != local_player_addr:
-                    ent = Entity(ent_addr, csgo_proc, game_module)
+                    try:
+                        ent = Entity(ent_addr, csgo_proc, game_module)
+                    except:
+                        continue
+                    
                     if not ent.dormant and ent.health > 0:
                         try:
                             ent.wts = wts_dx(overlay, view_matrix, ent.pos)
