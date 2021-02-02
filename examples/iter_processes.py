1f698be18282dc3dec096b2864bfdfd4202deb26
Shucks <shucks@web.com>
2021-02-02 23:51:26 +0100

    calculating module size

--- examples/iter_processes.py
+++ examples/iter_processes.py
@@ -3,4 +3,4 @@ from pymeow import enumerate_processes
 for proc in enumerate_processes():
     print(f"[{proc['name']}] Process ID: {proc['pid']}")
     for module, module_data in proc["modules"].items():
-        print(f"\t{module}: {hex(module_data['baseaddr'])}")
+        print(f"\t{module}: {hex(module_data['baseaddr'])} - {hex(module_data['baseaddr'] + module_data['basesize'])}")
